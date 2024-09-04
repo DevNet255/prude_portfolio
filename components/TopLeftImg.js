62aca3de559c43387f2ea923133ffe9bb48a6c48
DevNet255 <danielsonprudence@yahoo.com>
2024-09-04 13:18:42 +0300

    TopLeftImg.js

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -4,7 +4,7 @@ const TopLeftImg = () => {
   return (
    <div className='absolute left-0 right-0 mix-blend-color-dodge z-10 w-[200px] 
    xL:w-[400px] opacity-50'>
-    <Image src='/top-left-img.png' width={400} height={400} alt='' />
+    <Image src='./top-left-img.png' width={400} height={400} alt='' />
     </div>
   );
 };
