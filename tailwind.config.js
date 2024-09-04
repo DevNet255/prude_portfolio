3a2a481a50caab381270b2a340fcfcc65039dca7
DevNet255 <danielsonprudence@yahoo.com>
2024-09-04 13:03:20 +0300

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       backgroundImage: {
         explosion: 'url("/prude_portfolio/bg-explosion.png")',
-        circles: 'url("/bg-circles.png")',
+        circles: 'url("/prude_portfolio/circles.png")',
         circleStar: 'url("/circle-star.svg")',
         site: 'url("/prude_portfolio/site-bg.svg")',
       },
